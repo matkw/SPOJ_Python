169af0ecd0e71ddb73932049f124ce9ed57aeb98
matkw <mateusz.j.kwiatkowski@gmail.com>
2021-04-07 15:04:00 +0200

    Add PP0601B Spoj solution

---
+++ PP0601B/main.py
@@ -0,0 +1,12 @@
+# PP0601B
+# n - range of number
+# x - divisible_number
+# y - indivisible_number
+t = int(input())
+for i in range(t):
+    n, x, y = map(int, input().split())
+    if n > x:
+        for j in range(x, n, x):
+            if j % y != 0:
+                print(j, " ", end='')
+        print()
